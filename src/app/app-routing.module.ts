26e8bc544640588de299849e76ebd90ee683bf42
spfellers <spf.github@gmail.com>
2019-05-15 01:24:37 -0400

    Moving things to feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { homedir } from 'os';
+import { HomeModule } from './features/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './features/home/home.module#HomeModule'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: false }
+      )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
