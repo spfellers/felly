4ac953237ba399a066302a37e6c0ccf9cf225789
spfellers <spf.github@gmail.com>
2019-06-11 21:48:45 -0500

    fixing css props for rows

--- src/app/features/tier-list/components/tier-list/tier-list.component.ts
+++ src/app/features/tier-list/components/tier-list/tier-list.component.ts
@@ -10,26 +10,13 @@ export class TierListComponent implements OnInit {
 
   public ranks = ['S', 'A', 'B', 'C', 'D', 'F'];
 
-  public tierList = {
-    S: ['TEST', 'WHALE', 'ASDF'],
-    A: ['gfrg', 'WHAdffgLE', 'ASsdDF'],
-    B: ['AESF', 'WHALEdfdf', 'ASrDF'],
-    C: ['sdf345', 'WHdfwALE', 'ASweDF'],
-    D: ['345', 'gfdfgWHALE', 'ASDsdfF'],
-    F: ['TE34345ST', 'eggdf', 'AwwerwerSDF'],
-  };
-
   public countries = [ 'australia', 'canada', 'china', 'france', 'germany', 'india', 'italy', 'south-korea', 'united-kingdom', 'united-states' ];
 
   constructor() { }
 
   ngOnInit() { }
 
-  public drop(e: CdkDragDrop<string[]>): void {
-    moveItemInArray(this.tierList, e.previousIndex, e.currentIndex);
-  }
-
-  public getImage(item) {
+  public getImage(item: any) {
     return `assets/countries/${item}.svg`
   }
 
