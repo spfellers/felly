52cdc1a571ad14453ebc5fc230f75f222d962084
spfellers <spf.github@gmail.com>
2019-06-13 21:15:30 -0500

    messed up file system

--- src/app/features/tier-list/components/tier-list/tier-list.component.ts
+++ src/app/features/tier-list/components/tier-list/tier-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ContentChildren, QueryList, forwardRef } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { readlinkSync } from 'fs';
 
 @Component({
   selector: 'fel-tier-list',
@@ -7,17 +8,39 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./tier-list.component.css']
 })
 export class TierListComponent implements OnInit {
-
+  public free = false;
   public ranks = ['S', 'A', 'B', 'C', 'D', 'F'];
 
   public countries = [ 'australia', 'canada', 'china', 'france', 'germany', 'india', 'italy', 'south-korea', 'united-kingdom', 'united-states' ];
-
+  public tierLists = {
+    pergatory: [ 'australia', 'canada', 'china', 'france', 'germany', 'india', 'italy', 'south-korea', 'united-kingdom', 'united-states' ],
+    S: [],
+    A: [],
+    B: [],
+    C: [],
+    D: [],
+    F: []
+  };
+  
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+   }
 
   public getImage(item: any) {
     return `assets/countries/${item}.svg`
+  }
+
+  public drop(event: CdkDragDrop<any>): void {
+    console.log(event);
+    console.log(this.tierLists[event.container.id]);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.tierLists[event.container.id], event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(this.tierLists[event.previousContainer.id], this.tierLists[event.container.id],
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 
 }
