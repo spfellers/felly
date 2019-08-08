aa0358d8655ed6b9ae8d24ebd5199b22adca0b6b
spfellers <spf.github@gmail.com>
2019-08-07 21:46:52 -0500

    reverting back to free move tier-list in master

--- src/app/features/tier-list/components/tier-list/tier-list.component.ts
+++ src/app/features/tier-list/components/tier-list/tier-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ContentChildren, QueryList, forwardRef } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { readlinkSync } from 'fs';
 
 @Component({
   selector: 'fel-tier-list',
@@ -8,39 +7,19 @@ import { readlinkSync } from 'fs';
   styleUrls: ['./tier-list.component.css']
 })
 export class TierListComponent implements OnInit {
-  public free = false;
   public ranks = ['S', 'A', 'B', 'C', 'D', 'F'];
+  public category = 'countries';
 
   public countries = [ 'australia', 'canada', 'china', 'france', 'germany', 'india', 'italy', 'south-korea', 'united-kingdom', 'united-states' ];
-  public tierLists = {
-    pergatory: [ 'australia', 'canada', 'china', 'france', 'germany', 'india', 'italy', 'south-korea', 'united-kingdom', 'united-states' ],
-    S: [],
-    A: [],
-    B: [],
-    C: [],
-    D: [],
-    F: []
-  };
   
   constructor() { }
-
-  ngOnInit() {
-   }
+  
+  ngOnInit() { }
 
   public getImage(item: any) {
-    return `assets/countries/${item}.svg`
+    return `assets/${this.category}/${item}.svg`
   }
 
-  public drop(event: CdkDragDrop<any>): void {
-    console.log(event);
-    console.log(this.tierLists[event.container.id]);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(this.tierLists[event.container.id], event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(this.tierLists[event.previousContainer.id], this.tierLists[event.container.id],
-                        event.previousIndex,
-                        event.currentIndex);
-    }
-  }
+  public drop(event: CdkDragDrop<any>): void { }
 
 }
